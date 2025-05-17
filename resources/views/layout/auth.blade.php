0ff7e04219c35e3fa273bdd0538d2c67fc7f20bc
Hasnain <hasnainasif151@gmail.com>
2025-05-17 23:17:10 +0500

    changhe in auth layout

--- resources/views/layout/auth.blade.php
+++ resources/views/layout/auth.blade.php
@@ -61,15 +61,6 @@
     </div>
     <!-- / Layout wrapper -->
 
-    <div class="buy-now">
-      <a
-        href="https://themeselection.com/products/sneat-bootstrap-html-admin-template/"
-        target="_blank"
-        class="btn btn-danger btn-buy-now"
-        >Upgrade to Pro</a
-      >
-    </div>
-
   @include('pages.layoutpages.script')
   </body>
 </html>
